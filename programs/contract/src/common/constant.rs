7d51903486c0ef1fe328b0e56375dc9647757394
minhbear <minhbeardev@gmail.com>
2026-05-10 14:37:19 +0700

    chore: update operator pubkey in dev

--- programs/contract/src/common/constant.rs
+++ programs/contract/src/common/constant.rs
@@ -9,7 +9,7 @@ pub mod operator {
   pub const OPERATOR_PUBKEY: Pubkey = pubkey!("FKEynBSYhSASsAdzQJ4Cwvf2X3vZs3UERhTj3jhtFVBM");
 
   #[cfg(feature = "dev")]
-  pub const OPERATOR_PUBKEY: Pubkey = pubkey!("FKEynBSYhSASsAdzQJ4Cwvf2X3vZs3UERhTj3jhtFVBM");
+  pub const OPERATOR_PUBKEY: Pubkey = pubkey!("9YXBDuAtAAJt2Lmayi71zB1La3pTmytYGtLP8m3wr6zk");
 
   #[cfg(not(any(feature = "dev", feature = "staging")))]
   pub const OPERATOR_PUBKEY: Pubkey = pubkey!("FKEynBSYhSASsAdzQJ4Cwvf2X3vZs3UERhTj3jhtFVBM");
